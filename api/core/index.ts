61d4ec5f111862293e15018da26a52af812e69bd
moribeiro <moribeiro@midgard.local>
2023-08-17 16:02:39 -0300

    Tudo certo

--- api/core/index.ts
+++ api/core/index.ts
@@ -23,14 +23,13 @@ api.post('/integracao', async (req, res) => {
 })
 
 api.get('/integracao/reset', async (req, res) => {
-	const { v_card } = req.body
 	const bodyData = { cadastro: [] as any }
 	axios.put("https://api.jsonstorage.net/v1/json/42fd61f0-4b50-4085-9d3b-b782b5f12341/42efa98a-5d08-4578-b569-0e9ddd282a20?apiKey=565deae6-50a4-4f37-a54a-21d60c2f54d8", bodyData, {
 		headers: {
 			'Content-Type': 'application/json'
 		}
 	})
-	res.json(v_card)
+	res.json({message: "Sucesso"})
 })
 
 
