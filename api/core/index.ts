1c7ca599bd6961f474a1755f2df036b5f8a4c5c5
moribeiro <moribeiro@midgard.local>
2023-08-17 16:48:25 -0300

    asdasd

--- api/core/index.ts
+++ api/core/index.ts
@@ -19,7 +19,7 @@ api.get('/integracao', async (req, res) => {
 	res.json(data)
 })
 
-api.get('/integracao', async (req, res) => {
+api.get('/status', async (req, res) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
