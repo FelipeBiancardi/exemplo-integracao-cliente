d8bf51e391da20263653795239d886891d95f8d1
Matheus Oliveira <matheusvp2@ucl.br>
2023-08-17 00:08:53 -0300

    Update Router Patch

--- api/core/index.ts
+++ api/core/index.ts
@@ -7,7 +7,7 @@ const api = createAPI();
 api.post('/integracao', async (req, res) => {
 	const { v_card } = req.body
 	const bodyData = { cadastro: [v_card] }
-	axios.patch("https://api.jsonstorage.net/v1/json/42fd61f0-4b50-4085-9d3b-b782b5f12341/42efa98a-5d08-4578-b569-0e9ddd282a20", bodyData, {
+	axios.patch("https://api.jsonstorage.net/v1/json/42fd61f0-4b50-4085-9d3b-b782b5f12341/42efa98a-5d08-4578-b569-0e9ddd282a20?apiKey=565deae6-50a4-4f37-a54a-21d60c2f54d8", bodyData, {
 		headers: {
 			'Content-Type': 'application/json'
 		}
