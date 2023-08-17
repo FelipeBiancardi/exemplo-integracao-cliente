6350b521024f239f825aef0fc7896cfb8af866d2
moribeiro <moribeiro@midgard.local>
2023-08-17 16:33:41 -0300

    aaaaaaa

--- api/core/index.ts
+++ api/core/index.ts
@@ -4,24 +4,23 @@ import axios from 'axios';
 
 const api = createAPI({});
 
-api.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('ChaveAleatoria', 'Testando');
-	res.cors({
-		origin: '*',
-		headers: 'Content-Type,Authorization',
-		methods: 'GET,POST,PUT,DELETE',
-	});
-	next();
-});
-
 
 api.get('/integracao', async (req, res) => {
+	res.cors({
+		origin: '*',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		credentials: true,
+	});
 	const { data } = await axios.get("https://api.jsonstorage.net/v1/json/42fd61f0-4b50-4085-9d3b-b782b5f12341/42efa98a-5d08-4578-b569-0e9ddd282a20")
 	res.json(data)
 })
 
 api.post('/integracao', async (req, res) => {
+	res.cors({
+		origin: '*',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		credentials: true,
+	});
 	const { v_card } = req.body
 	const bodyData = { cadastro: [v_card] }
 	axios.patch("https://api.jsonstorage.net/v1/json/42fd61f0-4b50-4085-9d3b-b782b5f12341/42efa98a-5d08-4578-b569-0e9ddd282a20?apiKey=565deae6-50a4-4f37-a54a-21d60c2f54d8", bodyData, {
@@ -33,6 +32,11 @@ api.post('/integracao', async (req, res) => {
 })
 
 api.get('/integracao/reset', async (req, res) => {
+	res.cors({
+		origin: '*',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		credentials: true,
+	});
 	const bodyData = { cadastro: [] as any }
 	axios.put("https://api.jsonstorage.net/v1/json/42fd61f0-4b50-4085-9d3b-b782b5f12341/42efa98a-5d08-4578-b569-0e9ddd282a20?apiKey=565deae6-50a4-4f37-a54a-21d60c2f54d8", bodyData, {
 		headers: {
