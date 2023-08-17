4bd6550701b5688614d66e27dcde0d010a504697
moribeiro <moribeiro@midgard.local>
2023-08-17 17:41:59 -0300

    asdasdasdasd

--- api/core/index.ts
+++ api/core/index.ts
@@ -5,30 +5,24 @@ import axios from 'axios';
 const api = createAPI({});
 
 
-const VERSION = "2.1.0"
+const VERSION = "2.2.0"
 
 function registerCors(res: Response) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	res.header('Access-Control-Allow-Origin', '*'); // Ou especifique um domínio específico aqui
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 	res.header('Version', VERSION)
 }
 
 
 api.get('/integracao', async (req, res) => {
 	const { data } = await axios.get("https://api.jsonstorage.net/v1/json/42fd61f0-4b50-4085-9d3b-b782b5f12341/42efa98a-5d08-4578-b569-0e9ddd282a20")
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-	res.header('Version', VERSION)
+	registerCors(res)
 	res.json(data)
 })
 
 api.get('/status', async (req, res) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-	res.header('Version', VERSION)
+	registerCors(res)
 	res.json({ version: VERSION })
 })
 
@@ -40,10 +34,7 @@ api.post('/integracao', async (req, res) => {
 			'Content-Type': 'application/json'
 		}
 	})
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-	res.header('Version', VERSION)
+	registerCors(res)
 	res.json(v_card)
 })
 
